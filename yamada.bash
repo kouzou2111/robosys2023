bed0167578362fa1dfa58fa218f07884e67c2185
kouzou2111 <byopo@bangban.uk>
2023-10-26 13:06:20 +0900

    recode

--- yamada.bash
+++ yamada.bash
@@ -1,10 +1,16 @@
 #!/bin/bash
+# SPDX-FileCopyrightText:2023 kouzou2111
+# SPDX-License-Identifier: BSD-3-clause
+
 ng () {
           echo ${1}行目が違うよ   #$1はngの1番目の引数
           ret=1                   #最後に返す終了ステータスをセット
   }
 ret=0
-a=山田
-[ "$a" = 上田 ]|| ng "$LINENO"                      
-[ "$a" = 山田 ]|| ng "$LINENO"
-exit $ret
+
+### 入出力テスト ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 14 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
+exit $res
