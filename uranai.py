5b7f462804fdcaf33fb50afef97b66c03f7178c4
kouzou2111 <byopo@bangban.uk>
2023-11-21 14:04:31 +0900

    add license

--- uranai.py
+++ uranai.py
@@ -1,5 +1,10 @@
+#!/bin/bash
+# SPDX-FileCopyrightText:2023 kouzou2111
+# SPDX-License-Identifier: BSD-3-clause
+
 import random
 def get_random_data(filename):
     with open(filename, 'r') as file:
         data = file.readlines()
         return random.choice(data)
+
