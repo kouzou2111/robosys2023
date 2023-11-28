33d58d318dbd891ea6a674cda38d4fd4d9a0029f
kouzou2111 <byopo@bangban.uk>
2023-11-28 15:42:13 +0900

    fin

--- osaisentest.bash
+++ osaisentest.bash
@@ -1,4 +1,4 @@
-#!/bin/bash -xv
+#!/usr/bin/bash  
 # SPDX-FileCopyrightText:2023 kouzou2111
 # SPDX-License-Identifier: BSD-3-clause
 ng () {
@@ -10,28 +10,30 @@ res=0
 ### 正常なインプット###
 
 out=$(./osaisen 1) #<-1~10000を入力、一例として1を入力。出力結果はランダム
- [ "${out}" = 'どうも' ] || ng ${LINENO}
-
-out=$(./osaisen ５０００)
- [ "${out}" =  ] || ng ${LINENO}
+ [ "${out}" = "\nどうも\n おみくじの結果：$omikuzi \n相性がいい星座：$seiza \n相性がいい血液型：$ketueki \nラッキーカラー：$color \nラッキーアイテム：$item" ] || ng ${LINENO} #<-出力結果が別のテーブルからランダムに出力されるためかテストが困難
+ #[ "${out}" = "おみくじの結果：$omikuzi" ] || ng ${LINENO}
+ #[ "${out}" = "相性がいい星座：$seiza" ] || ng ${LINENO}
+ #[ "${out}" = "相性がいい血液型：$ketueki" ] || ng ${LINENO}
+ #[ "${out}" = "ラッキーカラー：$color" ] || ng ${LINENO}
+ #[ "${out}" = "ラッキーアイテム：$item" ] || ng ${LINENO}
 
 ### 間違ったインプット###
 
 out=$(./osaisen nisesatu)
- [ "$?" = 1 ] || ng ${LINENO}
- [ "${out}" = 'ごみをいれるな!' ] || ng ${LINENO}
-
-out=$(echo ./osaisen 子供銀行借用証)
- [ "$?" = 1 ] || ng ${LINENO}
- [ "${out}" = 'ごみをいれるな!' ] || ng ${LINENO}
-
-out=$(echo ./osaisen 1000円)
- [ "$?" = 1 ] || ng ${LINENO}
- [ "${out}" = 'ごみをいれるな!' ] || ng ${LINENO}
-
-out=$(echo ./osaisen 10001)
  [ "$?" = 0 ] || ng ${LINENO}
- [ "${out}" = 'こんなにもらえません!!' ] || ng ${LINENO}
+ [ "$out" = "ごみをいれるな!" ] || ng ${LINENO}
+
+out=$(./osaisen 子供銀行借用証)
+ [ "$?" = 0 ] || ng ${LINENO}
+ [ "$out" = "ごみをいれるな!" ] || ng ${LINENO}
+
+out=$(./osaisen 1000円)
+ [ "$?" = 0 ] || ng ${LINENO}
+ [ "$out" = "ごみをいれるな!" ] || ng ${LINENO}
+
+out=$(./osaisen 10001)
+ [ "$?" = 1 ] || ng ${LINENO}
+ [ "$out" = "こんなにもらえません!!" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
--- test.bash
+++ test.bash
@@ -1,3 +1,16 @@
 #!/bin/bash
+# SPDX-FileCopyrightText:2023 kouzou2111
+# SPDX-License-Identifier: BSD-3-clause
 
-seq 5 | ./plus
+ng () {
+          echo ${1}行目が違うよ   #$1はngの1番目の引数
+          ret=1                   #最後に返す終了ステータスをセット
+  }
+ret=0
+
+### 入出力テスト ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 14 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
+exit $res
