107318ada159c43a824fa75266666c8edd1805fa
kouzou2111 <byopo@bangban.uk>
2023-11-30 10:31:00 +0900

    fin

--- osaisentest.bash
+++ osaisentest.bash
@@ -1,8 +1,8 @@
-#!/usr/bin/bash  
+#!/usr/bin/bash 
 # SPDX-FileCopyrightText:2023 kouzou2111
 # SPDX-License-Identifier: BSD-3-clause
 ng () {
-      echo NG at Line ${1}
+      echo NG at Line $1
       rtn=1
 }
 rtn=0
@@ -10,30 +10,29 @@ rtn=0
 ### 正常なインプット###
 
 out=$(./osaisen 1) #<-1~10000を入力、一例として1を入力。出力結果はランダム
- [ "${out}" = "\nどうも\n おみくじの結果：$omikuzi \n相性がいい星座：$seiza \n相性がいい血液型：$ketueki \nラッキーカラー：$color \nラッキーアイテム：$item" ] || ng ${LINENO} #<-出力結果が別のテーブルからランダムに出力されるためかテストが困難
- #[ "${out}" = "おみくじの結果：$omikuzi" ] || ng ${LINENO}
- #[ "${out}" = "相性がいい星座：$seiza" ] || ng ${LINENO}
- #[ "${out}" = "相性がいい血液型：$ketueki" ] || ng ${LINENO}
- #[ "${out}" = "ラッキーカラー：$color" ] || ng ${LINENO}
- #[ "${out}" = "ラッキーアイテム：$item" ] || ng ${LINENO}
+ #<-出力結果が別のテーブルからランダムに出力されるためかテストが困難
 
 ### 間違ったインプット###
 
 out=$(./osaisen nisesatu)
  [ "$?" = 0 ] || ng ${LINENO}
- [ "$out" = "ごみをいれるな!" ] || ng ${LINENO}
+ [ "$out" =  '
+ごみをいれるな!' ] || ng ${LINENO}
 
 out=$(./osaisen 子供銀行借用証)
  [ "$?" = 0 ] || ng ${LINENO}
- [ "$out" = "ごみをいれるな!" ] || ng ${LINENO}
+ [ "$out" =  '
+ごみをいれるな!' ] || ng ${LINENO}
 
 out=$(./osaisen 1000円)
  [ "$?" = 0 ] || ng ${LINENO}
- [ "$out" = "ごみをいれるな!" ] || ng ${LINENO}
+ [ "$out" =  '
+ごみをいれるな!' ] || ng ${LINENO}
 
 out=$(./osaisen 10001)
- [ "$?" = 1 ] || ng ${LINENO}
- [ "$out" = "こんなにもらえません!!" ] || ng ${LINENO}
+ [ "$?" = 0 ] || ng ${LINENO}
+ [ "$out" =  '
+こんなにもらえません!!' ] || ng ${LINENO}
 
 [ "$rtn" = 0 ] && echo OK
 exit $rtn
