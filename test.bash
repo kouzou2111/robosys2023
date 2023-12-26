e1f9a9d7cf5a48a73b1f313356ce262c680ac6ff
kouzou2111 <byopo@bangban.uk>
2023-12-26 16:08:45 +0900

    fix

--- test.bash
+++ test.bash
@@ -1,27 +1,24 @@
 #!/bin/bash
 #SPDX-FileCopyrightText: 2023 kouzou2111
 #SPDX-License-Identifier: BSD-3-clause
-
 ng () {
-          echo ${1}行目が違うよ   #$1はngの1番目の引数
-          ret=1                   #最後に返す終了ステータスをセット
-  }
-ret=0
+	echo NG at Line $1
+	rtn=1
+}
+rtn=0
 
 ### 正常 ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
-
 ### 異常 ###
 out=$(echo あ | ./plus)
- [ "$?" = 1 ] || ng ${LINENO}
- [ "${out}" = "" ] || ng ${LINENO}
-    　
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 out=$(echo | ./plus)
- [ "$?" = 1 ] || ng ${LINENO}
- [ "${out}" = "" ] || ng ${LINENO}
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
-
-[ "$ret" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
-exit $ret
+[ "$ret" = 0 ] && echo OK
+exit $rtn
